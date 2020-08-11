3175a2dfc17fbd876cf38842c9365ebd629435d1
Manuel Hartwig <hartwig.manuel@web.de>
2020-08-11 19:42:08 +0200

    Added KZ scene

---
+++ Rupert Mayer - Konzept 2/Assets/04_scripts/LightFadeInOnEnable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFadeInOnEnable : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Light myLight;
+    public float fadeSpeed;
+    private float lightIntensity;
+
+    void Start()
+    {
+     myLight = GetComponent<Light>();  
+    lightIntensity = myLight.intensity;
+     myLight.intensity = 0; 
+    }
+    void OnEnable() {
+
+      myLight.intensity = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      if (myLight.intensity <= lightIntensity) {
+
+          myLight.intensity += 1 * fadeSpeed;
+      }  
+    }
+}
