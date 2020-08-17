f508c4bf144a0de417447caeb347993558f526d5
Manuel Hartwig <hartwig.manuel@web.de>
2020-08-18 00:57:44 +0200

    animations
    added preKZ scene; added character to sondergericht scene

---
+++ Rupert Mayer - Konzept 2/Assets/04_scripts/Sondergericht/Reposition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+
+    private Vector3 OrigPosition;
+    private float OrigRotY;
+    public GameObject CollisionObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      OrigPosition  =  transform.position;
+      OrigRotY = transform.rotation.y;
+    }
+
+   void OnCollisionEnter(Collision col) {
+       
+       if (col.gameObject.name == CollisionObject.name) {
+
+           transform.position = OrigPosition;
+           transform.Rotate(0, OrigRotY, 0);
+       }
+       
+   }
+}
---
+++ Rupert Mayer - Konzept 2/Assets/04_scripts/TimeBasedSceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeBasedSceneChange : MonoBehaviour
+{
+    private float Timer;
+    public float OffsetTime;
+    public int SceneNumber;
+    public void ChangeTheScene (int ChangeToScene) {
+        SceneManager.LoadScene(ChangeToScene);
+
+    }
+
+    void Update() {
+        Timer += Time.deltaTime;
+
+        if (Timer > OffsetTime) {
+
+            ChangeTheScene(SceneNumber);
+        }
+
+    }
+}
